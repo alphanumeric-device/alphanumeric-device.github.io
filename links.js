e778641140d7ae2c4ea4bf0afdac4191cbeed195
alphanumeric-device <112668163+alphanumeric-device@users.noreply.github.com>
2026-08-26 08:46:03 -0500

    Add files via upload

--- links.js
+++ links.js
@@ -2,12 +2,17 @@ var nav = document.getElementById('nav')
 var links = ['./page_files/page1.html']
 
 window.onload = ()=>{
+    
     links.forEach((link,i)=>{
-        var li = document.createElement('li')
-        var a = document.createElement('a')
-        a.href = link
-        a.textContent = link.replace('./page_files/', '')
-        li.appendChild(a)
-        nav.appendChild(li)
+        if(window.location.href.includes('link')){
+            console.log('Skipping file')
+        } else {
+            var li = document.createElement('li')
+            var a = document.createElement('a')
+            a.href = link
+            a.textContent = link.replace('./page_files/', '')
+            li.appendChild(a)
+            nav.appendChild(li)
+        }
     })
 }
