76b0249cac5581646ffaff77aaf922943f669ac8
alphanumeric-device <112668163+alphanumeric-device@users.noreply.github.com>
2026-08-26 08:41:19 -0500

    Add files via upload

---
+++ links.js
@@ -0,0 +1,13 @@
+var nav = document.getElementById('nav')
+var links = ['./page_files/page1.html']
+
+window.onload = ()=>{
+    links.forEach((link,i)=>{
+        var li = document.createElement('li')
+        var a = document.createElement('a')
+        a.href = link
+        a.textContent = link.replace('./page_files/', '')
+        li.appendChild(a)
+        nav.appendChild(li)
+    })
+}
