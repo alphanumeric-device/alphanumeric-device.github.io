f32e2a8f19bf51943b040ede744730bc7d75161f
alphanumeric-device <112668163+alphanumeric-device@users.noreply.github.com>
2026-08-26 09:02:08 -0500

    Add files via upload

--- links.js
+++ links.js
@@ -1,16 +1,20 @@
 var nav = document.getElementById('nav')
-var links = ['./index.html', './page_files/index.html']
+var links = ['../index.html', './page_files/index.html']
 
 window.onload = ()=>{
     
     links.forEach((link,i)=>{
-        if(window.location.href.includes('link')){
+        var href = window.location.href
+        var mod_href = link.includes('../')?link.replace('../', ''):link.replace('./', '')
+        if(mod_href.includes(link)){
             console.log('Skipping file')
         } else {
             var li = document.createElement('li')
             var a = document.createElement('a')
             a.href = link
-            a.textContent = link.replace('./page_files/', '')
+            var s = link.replace('./', '')
+           
+            a.textContent = s//s.replace('page_files/', '')
             li.appendChild(a)
             nav.appendChild(li)
         }
