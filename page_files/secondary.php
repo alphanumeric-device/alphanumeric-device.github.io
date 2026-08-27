43e7ad85d0bc4f560c9acaeb5cf548e234d14a89
alphanumeric-device <112668163+alphanumeric-device@users.noreply.github.com>
2026-08-27 08:32:38 -0500

    Add files via upload

---
+++ page_files/secondary.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page One</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <nav id = 'nav'>
+
+        </nav>
+        <h1>Code Transfer</h1>
+        <form action="./secondary.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+        <?php
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            // Check if image file is a actual image or fake image
+            if(isset($_POST["submit"])) {
+            // $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            // if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            // } else {
+            //     echo "File is not an image.";
+            //     $uploadOk = 0;
+            // }
+            }
+        ?>
+        <script src = '/links.js'></script>
+    </body>
+</html>
