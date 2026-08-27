bdfba45070a440598e13ea61bd801b5d74907c43
alphanumeric-device <112668163+alphanumeric-device@users.noreply.github.com>
2026-08-27 08:34:50 -0500

    Add files via upload

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 var nav = document.getElementById('nav')
-var links = ['/index.html', '/page_files/index.html']
+var links = ['/index.html', '/page_files/index.html', '/page_files/secondary.php']
 
 window.onload = ()=>{
     
