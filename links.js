21456b9d4a6342e774bc528f1d140e235b3d0d3c
alphanumeric-device <112668163+alphanumeric-device@users.noreply.github.com>
2026-08-27 09:14:40 -0500

    Add files via upload

--- links.js
+++ links.js
@@ -15,6 +15,17 @@ window.onload = ()=>{
         console.log(mod_href)
         if(mod_href==link){//needs to ignore linking the page if it is the page itself
             console.log('Skipping file')
+            var li = document.createElement('li')
+            var a = document.createElement('a')
+            a.href = link
+            a.disabled
+            a.style.color = 'red'
+
+            var s = link.replace('/', '')
+            //use at to change the path to a relative path from the file so that it works on local and on github
+            a.textContent = s //s.replace('page_files/', '')
+            li.appendChild(a)
+            nav.appendChild(li)
         } else {
             var li = document.createElement('li')
             var a = document.createElement('a')
